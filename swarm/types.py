1b99072cbae3a102565e119a788f8a44a5b13713
Cole Medin <47287758+coleam00@users.noreply.github.com>
2024-10-29 11:32:37 -0500

    Updated OpenAI's Swarm to use Ollama models

--- swarm/types.py
+++ swarm/types.py
@@ -13,10 +13,10 @@ AgentFunction = Callable[[], Union[str, "Agent", dict]]
 
 class Agent(BaseModel):
     name: str = "Agent"
-    model: str = "gpt-4o"
+    model: str = "qwen2.5-coder:7b"
     instructions: Union[str, Callable[[], str]] = "You are a helpful agent."
     functions: List[AgentFunction] = []
-    tool_choice: str = None
+    tool_choice: str = "auto"
     parallel_tool_calls: bool = True
 
 
